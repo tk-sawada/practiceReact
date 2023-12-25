3d3b89f319a54a21e582fe4f56c58278b1362d41
“tk-sawada” <tak8.sawada@gmail.com>
2023-12-25 17:13:57 +0900

    First commit

---
+++ src/index.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+//import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+/*
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+*/
+
+setInterval(() =>{
+  root.render(
+    <>
+      <p>Hello world</p>
+      <p>現在時刻: {(new Date()).toLocaleString()}</p>
+    </>
+  );
+}, 1000);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
+reportWebVitals();
