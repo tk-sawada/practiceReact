0678e8470f937568551baa77f31a928eaabae6f4
“tk-sawada” <tak8.sawada@gmail.com>
2023-12-29 17:59:45 +0900

    edit index.js

--- src/index.js
+++ src/index.js
@@ -6,6 +6,50 @@ import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
+root.render(
+  <p>ああ</p>
+)
+
+// P.79
+/*
+const title = 'ここれからはじめるReact実践入門';
+
+//createElementメソッドを使った場合
+root.render(
+  //上位の<div>要素を生成
+  React.createElement(
+    'div',
+    {className: 'main'},
+    //小要素の<p>、<img>、テキストを列挙
+    React.createElement(
+      'p',
+      null,
+      `「${title}(SBクリエイティブ刊)`
+    ),
+    React.createElement(
+      'img',
+      {
+        src: 'https://wings.msn.to/books/978-4-8156-1948-0/978-4-8156-1948-0.jpg',
+        alt: title
+      }
+    ), 
+    '絶賛発売中！'
+  )
+)
+*/
+//createElementメソッドを使わない場合
+/*
+root.render(
+  <div className="main">
+    <p> 「{title}」(SBクリエイティブ刊)</p>
+    <img src="https://wings.msn.to/books/978-4-8156-1948-0/978-4-8156-1948-0.jpg"
+        alt={title} />
+    絶賛発売中！
+  </div>
+);
+*/
+
+/*
 const props = {
   clor: 'White',
   backgroundColor: 'Blue',
@@ -15,6 +59,7 @@ const props = {
 root.render(
   <p style={props}>WINGSプロジェクト</p>
 );
+*/
 
 /*
 const attrs = {
